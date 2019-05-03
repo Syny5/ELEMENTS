cff0947df2e1e4625d3010c674c920bf25ea1c0c
syn <vicdemilly@gmail.com>
2019-05-03 12:01:31 +0200

    Ajout fonction vider panier

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 $(function(){
+  // On cache le contenu de la boutique (bdd html)
   $('h3').hide();
   $('.prix2').hide();
   $('.toHide').hide();
   $('#order2').hide();
+  // On vide le contenu du panier et on rafraichit la page.
   $('#emptyCart').on('click', function(){
     $('.panel-body').empty();
     location.reload();
